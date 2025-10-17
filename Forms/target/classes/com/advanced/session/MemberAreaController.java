c8301423d24b9c564e440075a0311d279eabec2b
adhambek010 <adhamsalaydinov5@gmail.com>
2025-10-17 22:04:06 +0200

    performance added

---
+++ Forms/target/classes/com/advanced/session/MemberAreaController.java
@@ -0,0 +1,24 @@
+package com.advanced.session;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class MemberAreaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public MemberAreaController() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
